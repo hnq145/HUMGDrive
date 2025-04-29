286ade38b0ee700767c2c5fe0fccd6e18a9044bb
Hoang Ngoc Quy <quyhoang1452003@gmail.com>
2025-04-29 11:07:20 +0700

    Version 1: Front-end, đã sửa các lỗi trong folder src

---
+++ .history/src/routers/AppRouter.d_20250429104518.ts
@@ -0,0 +1,2 @@
+declare const AppRouter: React.ComponentType;
+export default AppRouter;
---
+++ .history/src/utils/sortBySwitchFolder_20250429103918.js
@@ -0,0 +1,16 @@
+const sortBySwitch = (sortBy) => {
+import default from './../../tailwind.config';
+    switch(sortBy) {
+
+        case "alp_asc":
+            return {name: 1}
+        case "alp_desc": 
+            return {name: -1}
+        case "date_asc":
+            return {createdAt: 1}
+        default:
+            return {createdAt: -1}
+    }
+}
+
+export default = sortBySwitch;
